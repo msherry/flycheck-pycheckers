4f65e93931c5be4b183d2a3cf5a52a394f9a09b7
Marc Sherry <msherry@gmail.com>
2018-08-15 14:55:16 -0700

    Version bump

--- flycheck-pycheckers.el
+++ flycheck-pycheckers.el
@@ -2,7 +2,7 @@
 
 ;; Copyright Marc Sherry <msherry@gmail.com>
 ;; Homepage: https://github.com/msherry/flycheck-pycheckers
-;; Version: 0.11.x
+;; Version: 0.11.0
 ;; Package-Requires: ((flycheck "0.18"))
 ;; Keywords: convenience, tools, languages
 
